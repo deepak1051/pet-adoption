2af48fcdd9661d6b0efe506ca6ed8f9180315d76
deepak1051 <lightyagamigoku@gmail.com>
2024-02-06 14:20:49 +0530

    pet data

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
     clone.querySelector('h3').textContent = pet.name;
+    clone.querySelector('.pet-description').textContent = pet.description;
+    clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear);
+    clone.querySelector('.pet-card-photo img').src = pet.photo;
+    clone.querySelector('.pet-card-photo img').alt = pet.name;
     console.log(clone);
     wrapper.appendChild(clone);
   });
@@ -30,3 +34,12 @@ async function petsArea() {
 }
 
 petsArea();
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+
+  if (age == 1) return '1 year old';
+  if (age == 0) return 'Less than a year old';
+  return `${age} years old`;
+}
